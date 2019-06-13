e69f621443041c74e4324918ee0d2c15aa346dff
Srikanth Davu <srikanth.davu@nasa.gov>
2019-06-13 16:16:16 -0400

    CMR-5631 Adding cmr_search_endpoint to test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  # # CMR API endpoint
+  config.cmr_search_endpoint = 'https://cmr.earthdata.nasa.gov/search'
 end
